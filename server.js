9f5f80fde5ea8c8eb01e0d4e842199c879b886a8
ralf <ralf_zonneveld@hotmail.com>
2021-04-29 19:11:50 +0200

    begun at rooms

--- server.js
+++ server.js
@@ -3,106 +3,37 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 5000;
+
 const { getData, getRecipeData } = require('./modules/fetch.js');
+const { idCreator } = require('./modules/uniqueID.js');
+
 const bodyParser = require('body-parser');
-const firebase = require('firebase');
-const uuid = require('uuid').v4;
-
-require('dotenv').config();
-
-firebase.initializeApp({
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-});
-
-app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.render('index.ejs');
-});
-
-// ! Emit this data+action to the other so user X knows the state of user Y // return io.emit('dataRecipe', dataRecipe);
-// app.post('/match/:id', async function (req, res) {
-//   let checkedRecipe = req.body.recipeID;
-
-//   let data = await getRecipeData(checkedRecipe);
-//   console.log('query data: ', data);
-
-//   // const db = firebase.firestore();
-//   // const dataCollection = db.collection('users').doc(nickname);
-
-//   // const update = await dataCollection.update({
-//   //   recipe: checkedRecipe,
-//   // });
-//   // update;
-
-//   res.render('pages/match.ejs', { data: data[0] });
-// });
-
-app.get('/match/:id', async function (req, res) {
-  let data = await getRecipeData(req.params.id);
-  console.log(data);
-  res.render('pages/match.ejs', { data: data[0] });
-});
-
-// if user LoggedIn=false, then redirect to login
-app.get('/login', function (req, res) {
   res.render('pages/login.ejs');
 });
 
-app.post('/login', async function (req, res) {
+app.post('/', async function (req, res) {
   const nickname = req.body.name;
-  const password = req.body.password;
-  console.log(nickname, password);
+  console.log(nickname);
 
-  const db = firebase.firestore();
-  const data = db.collection('users');
+  let roomId = idCreator(8);
 
-  const snapshot = await data
-    .where('username', '==', nickname.toLowerCase())
-    .where('password', '==', password)
-    .get();
-
-  snapshot.forEach((doc) => {
-    console.log(doc.data());
-  });
-
-  if (snapshot.empty) {
-    console.log('No user found');
-    // send alert (validate)
-    res.redirect('/login');
-  }
-
-  res.redirect('/');
+  res.redirect(`/${roomId}`);
 });
 
-app.get('/room', function (req, res) {
-  res.render('pages/room.ejs');
+app.get('/:roomId', function (req, res) {
+  let roomId = req.params.roomId;
+  console.log(roomId);
+  res.render('index.ejs', { id: roomId });
 });
 
-app.get('/register', function (req, res) {
-  res.render('pages/register.ejs');
-});
-
-app.post('/register', async function (req, res) {
-  const nickname = req.body.name;
-  const password = req.body.password;
-
-  const db = firebase.firestore();
-  const data = db.collection('users');
-
-  const snapshot = await data.where('username', '==', nickname).get();
-
-  if (snapshot.empty) {
-    await data.doc(uuid()).set({
-      username: nickname.toLowerCase(),
-      password: password,
-    });
-  }
-
-  res.redirect('/login');
+app.get('/:roomId/match/:recipeId', async function (req, res) {
+  let data = await getRecipeData(req.params.recipeId);
+  console.log(data);
+  res.render('pages/match.ejs', { data: data[0] });
 });
 
 io.on('connection', (socket) => {
@@ -118,6 +49,7 @@ io.on('connection', (socket) => {
   socket.on('chosenRecipe', (recipeID) => {
     io.emit('chosenRecipe', recipeID);
     getDataOfRecipe(recipeID);
+    console.log(recipeID);
   });
 
   // Message handler
@@ -144,7 +76,7 @@ io.on('connection', (socket) => {
 
     // return emitted data for clientside handling for the other client
     // return socket.broadcast.emit('dataRecipe', dataRecipe);
-    return io.emit('dataRecipe', dataRecipe);
+    return socket.emit('dataRecipe', dataRecipe);
   }
 });
 
